98ebd00a1aa6486f95ee25940d3c91437d0c0f52
Sean Ephraim <sean.ephraim2022@campus-eni.fr>
2022-07-19 12:30:27 +0200

    Création des fixtures utilisateurs pour test le logins +  Header, footer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,16 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("", name="main_")
+ */
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="app_main")
+     * @Route("", name="home")
      */
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        return $this->redirectToRoute('app_login');
+    }
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(): Response
+    {
+        return $this->render('main/index.html.twig' , [
         ]);
     }
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Campus;
+use App\Entity\Participant;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private Generator $generator;
+    private ObjectManager $manager;
+    private UserPasswordHasherInterface $hasher;
+
+    public function load(ObjectManager $manager): void
+    {
+        // functions ADD
+        $this->manager = $manager;
+        $this->addCampus();
+        $this->addUsers();
+        $manager->flush();
+    }
+
+    public function __construct(UserPasswordHasherInterface $hasher){
+        $this->generator = Factory::create("fr_FR");
+        $this->hasher = $hasher;
+    }
+
+    public function addCampus() {
+        $campusList = ["Rennes", "Nantes", "Niort", "Quimper"];
+        foreach ($campusList as $campusName) {
+            $campusObject = new Campus();
+            $campusObject->setNom($campusName);
+            $this->manager->persist($campusObject);
+        }
+        $this->manager->flush();
+    }
+    public function addUsers() {
+        $campusList = $this->manager->getRepository(Campus::class)->findAll();
+        for ($i=0 ; $i < 10; $i++) {
+            $utilisateur = new Participant();
+            $utilisateur->setRoles(["ROLE_USER"]);
+            $utilisateur->setActif(true);
+            $utilisateur->setCampus($this->generator->randomElement($campusList));
+            $utilisateur->setMail($this->generator->email);
+            $utilisateur->setNom($this->generator->lastName);
+            $utilisateur->setPrenom($this->generator->firstName);
+            $utilisateur->setTelephone($this->generator->phoneNumber);
+
+            $password = $this->hasher->hashPassword($utilisateur, $this->generator->password);
+            $utilisateur->setPassword($password);
+            $this->manager->persist($utilisateur);
+        }
+
+        $utilisateur = new Participant();
+        $utilisateur->setRoles(["ROLE_USER"]);
+        $utilisateur->setActif(true);
+        $utilisateur->setCampus($this->generator->randomElement($campusList));
+        $utilisateur->setMail($this->generator->email);
+        $utilisateur->setNom($this->generator->lastName);
+        $utilisateur->setPrenom($this->generator->firstName);
+        $utilisateur->setTelephone($this->generator->phoneNumber);
+
+        $password = $this->hasher->hashPassword($utilisateur, "testtest");
+        $utilisateur->setPassword($password);
+        $this->manager->persist($utilisateur);
+
+        $this->manager->flush();
+    }
+}
