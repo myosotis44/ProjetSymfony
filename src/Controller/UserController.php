247ddc47c8525fac01511c57a7b00658b31f5a77
Victoria Gouin <victoria.gouin@gmail.com>
2022-07-25 11:21:12 +0200

    Création d'une instance de File pour régler le problème d'édition d'une image

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -30,12 +31,15 @@ class UserController extends AbstractController
                                  UserPasswordHasherInterface $userPasswordHasher, int $id): Response
     {
         $user = $participantRepository->find($id);
+        $user->setImageUtilisateur(
+            new File($this->getParameter('images_directory').'/'.$user->getImageUtilisateur())
+        );
         $userForm = $this->createForm(UserType::class, $user);
         $userForm->handleRequest($request);
 
-        $confirmPswd = $user->getPassword() === $userForm->get('confirmation')->getData();
+        //$confirmPswd = $user->getPassword() === $userForm->get('confirmation')->getData();
 
-        if ($userForm->isSubmitted() && $userForm->isValid() && $confirmPswd) {
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -52,8 +56,7 @@ class UserController extends AbstractController
                $safeFilename = $slugger->slug($originalFilename);
                 $newFilename = $safeFilename . '-' . uniqid() . '.' . $eventImage->guessExtension();
 
-
-                // Move the file to the directory where images are stored
+                    // Move the file to the directory where images are stored
                     try {
                         $eventImage->move(
                             $this->getParameter('images_directory'),
