5e4069915e155541c6804a8f8f57f3f78ef31767
cvernet35 <github@suppinfo.fr>
2022-07-26 16:10:59 +0200

    Modif MainController suite pb d'accès site

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ use App\Form\Model\OutFilterFormModel;
 use App\Form\OutFilterFormType;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,23 +19,10 @@ class MainController extends AbstractController
     /**
      * @Route("", name="home")
      */
-    public function home(Request $request,SortieRepository $sortieRepository): Response
-    {
-        $filteredOuts = null;
-        $filter = new OutFilterFormModel();
-        $filterForm = $this->createForm(OutFilterFormType::class, $filter);
-
-        $filterForm->handleRequest($request);
-
-        if ($filterForm->isSubmitted() && $filterForm->isValid()) {
-            $filteredOuts = $sortieRepository->outFilterDQLGenerator($filter);
+        public function home(): Response
+        {
+            return $this->redirectToRoute('out_index');
         }
-
-        return $this->render('out/index.html.twig', [
-            'filterForm' => $filterForm->createView(),
-            'filteredOuts' => $filteredOuts
-        ]);
-    }
 
     /**
      * @Route("/test", name="test")
