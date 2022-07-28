fcbe645b9580e82b0010c82e708c689b0f11e769
cvernet35 <github@suppinfo.fr>
2022-07-28 09:44:01 +0200

    modifier une annonce + cycle de vie d'une sortie

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -94,29 +94,4 @@ class SortieRepository extends ServiceEntityRepository
         return ($query->getResult());
 
     }
-
-//    /**
-//     * @return Sortie[] Returns an array of Sortie objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Sortie
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
--- src/Services/OutServices.php
+++ src/Services/OutServices.php
@@ -2,27 +2,28 @@
 
 namespace App\Services;
 
+use App\Repository\EtatRepository;
+use App\Repository\SortieRepository;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class OutServices
 {
+    protected $etatRepository;
+    protected $sortieRepository;
+
+    public function __construct(EtatRepository $etatRepository, SortieRepository $sortieRepository) {
+        $this->etatRepository = $etatRepository;
+        $this->sortieRepository = $sortieRepository;
+    }
+
     public function actionsFilter($filteredOuts, UserInterface $connectedUser) {
 
         foreach ($filteredOuts as $eachFilteredOut) {
 
-            $nbParticipants = 0;
-            $outRegisteredUser = false;
+            $outRegisteredUser = $eachFilteredOut->getParticipants()->contains($connectedUser);
             $actions = array();
 
-
-            foreach ($eachFilteredOut->getParticipants() as $eachParticipant) {
-                $nbParticipants ++;
-                if ($eachParticipant == $connectedUser) {
-                    $outRegisteredUser = true;
-                }
-            }
-
-            if ($eachFilteredOut->getEtat()->getLibelle() === 'En création'
+          if ($eachFilteredOut->getEtat()->getLibelle() === 'En création'
                 && $eachFilteredOut->getOrganisateur() == $connectedUser) {
                 $actions[] = 'Modifier';
                 $actions[] = 'Publier';
@@ -32,12 +33,18 @@ class OutServices
             }
 
             if ($eachFilteredOut->getEtat()->getLibelle() === 'Ouverte') {
-                if ($outRegisteredUser) {
-                    $actions[] = 'Se désister';
+
+                if (new \DateTime() > $eachFilteredOut->getDateLimiteInscription()) {
+
+                    $eachFilteredOut->setEtat($this->etatRepository->findOneBy(['libelle'=>'Clôturée']));
+                    $this->sortieRepository->add($eachFilteredOut, true);
                 }
                 else {
                     if ($eachFilteredOut->getOrganisateur() == $connectedUser) {
                         $actions[] = 'Annuler';
+                    }
+                    if ($outRegisteredUser) {
+                        $actions[] = 'Se désister';
                     }
                     else {
                         $actions[] = 'S\'inscrire';
@@ -45,16 +52,90 @@ class OutServices
                 }
             }
 
-            if ($eachFilteredOut->getEtat()->getLibelle() === 'Clôturée' && $outRegisteredUser) {
-                $actions[] = 'Se désister';
+            if ($eachFilteredOut->getEtat()->getLibelle() === 'Clôturée') {
+
+                if ( new \DateTime() > $eachFilteredOut->getDateHeureDebut()) {
+                    $eachFilteredOut->setEtat($this->etatRepository->findOneBy(['libelle'=>'Activité en cours']));
+                    $this->sortieRepository->add($eachFilteredOut, true);
+                }
+                else {
+                    if ($outRegisteredUser) {
+                        $actions[] = 'Se désister';
+                    }
+                }
             }
 
-            $eachFilteredOut->setNbParticipants($nbParticipants);
+            if ($eachFilteredOut->getEtat()->getLibelle() === 'Activité en cours') {
+
+/**/                if ( new \DateTime() > $eachFilteredOut->getDateHeureDebut()) { // + $eachFilteredOut->getDuree()) {
+                    $eachFilteredOut->setEtat($this->etatRepository->findOneBy(['libelle'=>'Activité Terminée']));
+                    $this->sortieRepository->add($eachFilteredOut, true);
+                }
+            }
+
+            $eachFilteredOut->setNbParticipants($eachFilteredOut->getParticipants()->count());
 
             $eachFilteredOut->setActions($actions);
+
         }
 
         return $filteredOuts;
 
     }
 }
+
+
+/*
+            switch ($eachFilteredOut->getEtat()->getLibelle()) {
+
+                case 'En création' :
+                    if ($eachFilteredOut->getOrganisateur() == $connectedUser) {
+                        $actions[] = 'Modifier';
+                        $actions[] = 'Publier';
+                    }
+                    else {
+                        $actions[] = 'Afficher';
+                    }
+                    break;
+
+                case 'Ouverte' :
+                    if (new \DateTime() > $eachFilteredOut->getDateLimiteInscription()) {
+                        $eachFilteredOut->setEtat($this->etatRepository->findOneBy(['libelle'=>'Clôturée']));
+                        $this->sortieRepository->add($eachFilteredOut, true);
+                    }
+                    else {
+                        if ($eachFilteredOut->getOrganisateur() == $connectedUser) {
+                            $actions[] = 'Annuler';
+                        }
+                        if ($outRegisteredUser) {
+                            $actions[] = 'Se désister';
+                        }
+                        else {
+                            $actions[] = 'S\'inscrire';
+                        }
+                    }
+                    break;
+
+                case 'Clôturée' :
+                    if (new \DateTime() > $eachFilteredOut->getDateHeureDebut()) {
+                        $eachFilteredOut->setEtat($this->etatRepository->findOneBy(['libelle'=>'Activité en cours']));
+                        $this->sortieRepository->add($eachFilteredOut, true);
+                    }
+                    else {
+                        if ($outRegisteredUser) {
+                            $actions[] = 'Se désister';
+                        }
+                    }
+                    break;
+
+                case 'Activité en cours' :
+               if (new \DateTime() > $eachFilteredOut->getDateHeureDebut()) { // + $eachFilteredOut->getDuree()) {
+    $eachFilteredOut->setEtat($this->etatRepository->findOneBy(['libelle'=>'Activité Terminée']));
+    $this->sortieRepository->add($eachFilteredOut, true);
+}
+break;
+
+}
+
+
+*/
